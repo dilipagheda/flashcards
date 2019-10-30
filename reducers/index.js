290ddbad70961899868bf1b8cafa732c0a6f0115
Dilip <dilip_agheda@yahoo.com>
2019-10-30 21:23:01 +1100

    updates to reducer

--- reducers/index.js
+++ reducers/index.js
@@ -1,44 +1,44 @@
 import { RECEIVE_DECKS, ADD_DECK, ADD_CARD, DELETE_DECK } from '../actions/types';
 
 const initialState = {
-	React: {
-		title: 'React',
-		questions: [
-			{
-				question: 'What is React?',
-				answer: 'A library for managing user interfaces'
-			},
-			{
-				question: 'Where do you make Ajax requests in React?',
-				answer: 'The componentDidMount lifecycle event'
-			}
-		]
-	},
-	JavaScript: {
-		title: 'JavaScript',
-		questions: [
-			{
-				question: 'What is a closure?',
-				answer:
-					'The combination of a function and the lexical environment within which that function was declared.'
-			}
-		]
-	}
+	// React: {
+	// 	title: 'React',
+	// 	questions: [
+	// 		{
+	// 			question: 'What is React?',
+	// 			answer: 'A library for managing user interfaces'
+	// 		},
+	// 		{
+	// 			question: 'Where do you make Ajax requests in React?',
+	// 			answer: 'The componentDidMount lifecycle event'
+	// 		}
+	// 	]
+	// },
+	// JavaScript: {
+	// 	title: 'JavaScript',
+	// 	questions: [
+	// 		{
+	// 			question: 'What is a closure?',
+	// 			answer:
+	// 				'The combination of a function and the lexical environment within which that function was declared.'
+	// 		}
+	// 	]
+	// }
 };
 
 function decksReducer(state = initialState, action) {
 	switch (action.type) {
 		case RECEIVE_DECKS:
-			return state;
+			return { ...action.decks };
 			break;
 		case ADD_DECK:
 			return { ...state, [action.title]: { title: action.title, questions: [] } };
 		case ADD_CARD:
 			return {
 				...state,
-				[action.card.id]: {
-					...state[action.card.id],
-					questions: [ ...state[action.card.id].questions, action.card ]
+				[action.id]: {
+					...state[action.id],
+					questions: [ ...state[action.id].questions, action.card ]
 				}
 			};
 		case DELETE_DECK:
