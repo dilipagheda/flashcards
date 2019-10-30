c36aeb399e952cd755f024b578289a737cf72f44
Dilip <dilip_agheda@yahoo.com>
2019-10-30 21:22:03 +1100

    refactoring actions

--- actions/index.js
+++ actions/index.js
@@ -1,4 +1,5 @@
 import { RECEIVE_DECKS, ADD_DECK, ADD_CARD, DELETE_DECK } from './types';
+import * as API from '../utils/api';
 
 export function receiveDecks(decks) {
 	return {
@@ -14,10 +15,11 @@ export function addDeck(title) {
 	};
 }
 
-export function addCard(card) {
+export function addCard(id, card) {
 	return {
 		type: ADD_CARD,
-		card
+		card,
+		id
 	};
 }
 
@@ -25,5 +27,41 @@ export function deleteDeck(title) {
 	return {
 		type: DELETE_DECK,
 		title
+	};
+}
+
+/* redux thunk functions */
+export function getDecks() {
+	return (dispatch) => {
+		return API.getDecks().then((decks) => {
+			const obj = JSON.parse(decks);
+			dispatch(receiveDecks(obj));
+		});
+	};
+}
+
+export function createNewDeck(title) {
+	return (dispatch) => {
+		return API.saveDeckTitle(title).then(() => {
+			dispatch(addDeck(title));
+		});
+	};
+}
+
+export function addNewCardToDeck(id, card) {
+	return (dispatch) => {
+		return API.addCardToDeck(id, card).then(() => {
+			dispatch(addCard(id, card));
+		});
+	};
+}
+
+export function deleteExistingDeck(id) {
+	return (dispatch) => {
+		return API.deleteDeck(id)
+			.then(() => {
+				dispatch(deleteDeck(id));
+			})
+			.catch((error) => console.log(error));
 	};
 }
