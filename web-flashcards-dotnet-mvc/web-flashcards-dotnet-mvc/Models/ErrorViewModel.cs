3914183671775230c825c5fa01254c3d682d8f41
Dilip <dilip_agheda@yahoo.com>
2019-12-24 21:06:59 +1100

    Created a template project for web version of flashcards using dotnet core mvc 3.0

---
+++ web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace web_flashcards_dotnet_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
