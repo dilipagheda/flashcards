94ca82c91be396bc89e00c0e4a790cd32d24010b
Dilip <dilip_agheda@yahoo.com>
2020-01-18 19:34:11 +1100

    Added Initial DbContext class for Flashcards requirements

---
+++ api-flashcards-dotnet/api-flashcards-dotnet/Data/FlashcardDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using api_flashcards_dotnet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_flashcards_dotnet.Data
+{
+    public class FlashcardDbContext:DbContext
+    {
+        public FlashcardDbContext(DbContextOptions<FlashcardDbContext> options)
+            : base(options)
+        { }
+
+        public DbSet<Deck> Decks { get; set; }
+        public DbSet<Card> Cards { get; set; }
+        public DbSet<Question> Questions { get; set; }
+    }
+}
