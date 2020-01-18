4ffe7e4aeb3753cc5e606938cd6004d7a14dd34a
Dilip <dilip_agheda@yahoo.com>
2020-01-18 21:32:27 +1100

    Added Dtos folder and classes for request and response - Decks route

---
+++ api-flashcards-dotnet/api-flashcards-dotnet/Dtos/DecksRequest.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace api_flashcards_dotnet.Dtos
+{
+    public class DecksRequest
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ api-flashcards-dotnet/api-flashcards-dotnet/Dtos/DecksResponse.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using api_flashcards_dotnet.Models;
+
+namespace api_flashcards_dotnet.Dtos
+{
+    public class DecksResponse
+    {
+        public List<Deck> Decks { get; set; }
+    }
+}
