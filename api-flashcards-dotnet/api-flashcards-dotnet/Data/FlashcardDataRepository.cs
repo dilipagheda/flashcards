36b4cf81dc96159a1a5564017e4a9be23cda9983
Dilip <dilip_agheda@yahoo.com>
2020-01-18 21:32:45 +1100

    Added basic flashcard repository

---
+++ api-flashcards-dotnet/api-flashcards-dotnet/Data/FlashcardDataRepository.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using api_flashcards_dotnet.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace api_flashcards_dotnet.Data
+{
+    public class FlashcardDataRepository
+    {
+        private readonly FlashcardDbContext _context;
+
+        public FlashcardDataRepository(FlashcardDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddDeck(string deckName)
+        {
+            Deck deck = new Deck()
+            {
+                Name = deckName
+            };
+            _context.Decks.Add(deck);
+            await _context.SaveChangesAsync();
+        }
+
+        public async Task<List<Deck>> GetAllDecks()
+        {
+           return await _context.Decks.ToListAsync();
+        }
+    }
+}
