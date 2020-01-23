7d7f308ef9f971ba0fd96e739e641caa36c0657b
Dilip <dilip_agheda@yahoo.com>
2020-01-23 20:10:05 +1100

    Added DecksController

--- api-flashcards-dotnet/api-flashcards-dotnet/Controllers/DecksController.cs
+++ api-flashcards-dotnet/api-flashcards-dotnet/Controllers/DecksController.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using api_flashcards_dotnet.Data;
+using api_flashcards_dotnet.Dtos;
 using api_flashcards_dotnet.Models;
+using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -15,17 +17,23 @@ namespace api_flashcards_dotnet.Controllers
     public class DecksController : Controller
     {
         private readonly FlashcardDataRepository _flashcardDataRepository;
+        private readonly IMapper _mapper;
 
-        public DecksController(FlashcardDataRepository flashcardDataRepository)
+        public DecksController(FlashcardDataRepository flashcardDataRepository, IMapper mapper)
         {
             _flashcardDataRepository = flashcardDataRepository;
+            _mapper = mapper;
         }
 
         [HttpGet]
         public async Task<ActionResult<List<Deck>>> Get()
         {
             List<Deck> _decks = await _flashcardDataRepository.GetAllDecks();
-            return Ok(_decks);
+
+
+            List<DeckResponse> _decksResponse =_mapper.Map<List<DeckResponse>>(_decks);
+
+            return Ok(_decksResponse);
         }
 
         [HttpPost]
