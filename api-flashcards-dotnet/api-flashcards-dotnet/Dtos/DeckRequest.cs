6bdb460a44dcdc5f6fb4dc0bc4dd46ab1d60b107
Dilip <dilip_agheda@yahoo.com>
2020-01-25 10:37:53 +1100

    Added validation attributes on DeckRequest

--- api-flashcards-dotnet/api-flashcards-dotnet/Dtos/DeckRequest.cs
+++ api-flashcards-dotnet/api-flashcards-dotnet/Dtos/DeckRequest.cs
@@ -1,8 +1,12 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
+
 namespace api_flashcards_dotnet.Dtos
 {
     public class DeckRequest
     {
+        [Required]
+        [StringLength(50, MinimumLength = 5)]
         public string Name { get; set; }
     }
 }
