90409ea43f6d7018b90c3610590f2ce84f37bcde
Dilip <dilip_agheda@yahoo.com>
2020-01-26 16:43:10 +1100

    Removed Name field from CardRequest

--- api-flashcards-dotnet/api-flashcards-dotnet/Dtos/CardRequest.cs
+++ api-flashcards-dotnet/api-flashcards-dotnet/Dtos/CardRequest.cs
@@ -6,10 +6,6 @@ namespace api_flashcards_dotnet.Dtos
     public class CardRequest
     {
         [Required]
-        [StringLength(150, MinimumLength = 5)]
-        public string Name { get; set; }
-
-        [Required]
         [StringLength(500, MinimumLength = 5)]
         public string QuestionText { get; set; }
 
