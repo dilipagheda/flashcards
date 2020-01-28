b2aaf2342e5b877bf611857a5ef61e18ac014ab6
Dilip <dilip_agheda@yahoo.com>
2020-01-28 22:09:37 +1100

    Added test for creating new deck

--- api-flashcards-dotnet/IntegrationTests/DecksControllerTests.cs
+++ api-flashcards-dotnet/IntegrationTests/DecksControllerTests.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using api_flashcards_dotnet;
 using api_flashcards_dotnet.Dtos;
@@ -51,6 +53,52 @@ namespace IntegrationTests
                     }
                 }
               );
+        }
+
+        [Fact]
+        public async Task Add_NewDeck_ReturnsSuccess_Adds_NewDeck()
+        {
+            // Arrange
+            var _client = _factory.CreateClient();
+
+
+            FlurlClient flurlClient = new FlurlClient(_client);
+
+            // Act
+            HttpResponseMessage _resp = await flurlClient.Request("/decks").PostJsonAsync(new DeckRequest() { Name = "React" });
+
+            //Assert
+            _resp.IsSuccessStatusCode.Should().BeTrue();
+            _resp.StatusCode.Should().Be(HttpStatusCode.Created);
+
+            DeckResponse _deckResponse = await flurlClient.Request("/decks").GetJsonAsync<DeckResponse>();
+
+            // Do GET call and assert that new deck is added and returned
+            _deckResponse.Decks.Count.Should().Be(3);
+            _deckResponse.Decks.Should().BeEquivalentTo(
+                new List<DeckResponseDto>()
+                {
+                    new DeckResponseDto()
+                    {
+                        Id = 1,
+                        Name = "JavaScript"
+                    },
+                    new DeckResponseDto()
+                    {
+                        Id=2,
+                        Name="C#"
+                    },
+                    new DeckResponseDto()
+                    {
+                        Id=3,
+                        Name="React"
+                    }
+                }
+              );
+            //Clean up using Delete call
+            _resp = await flurlClient.Request("/decks/3").DeleteAsync();
+            _resp.IsSuccessStatusCode.Should().BeTrue("Failed to delete deck id 3");
+
         }
     }
 }
