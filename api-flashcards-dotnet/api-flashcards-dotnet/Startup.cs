6594af7dbf3188ef617e70a087ccccef985f845b
Dilip <dilip_agheda@yahoo.com>
2020-02-13 22:47:01 +1100

    Added services for identity and security

--- api-flashcards-dotnet/api-flashcards-dotnet/Startup.cs
+++ api-flashcards-dotnet/api-flashcards-dotnet/Startup.cs
@@ -1,19 +1,28 @@
 using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using api_flashcards_dotnet.Data;
+using api_flashcards_dotnet.Infrastructure;
+using api_flashcards_dotnet.Models;
 using api_flashcards_dotnet.Profiles;
 using AutoMapper;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.IdentityModel.Tokens;
 
 namespace api_flashcards_dotnet
 {
@@ -29,13 +38,40 @@ namespace api_flashcards_dotnet
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddControllers();
             services.AddDbContext<FlashcardDbContext>(options =>
                 options.UseSqlite("Data Source=flashcards.db"));
             services.AddScoped<IFlashcardDataRepository, FlashcardDataRepository>();
 
-
             services.AddAutoMapper(typeof(Startup));
+
+            services.AddIdentityCore<ApplicationUser>()
+                .AddEntityFrameworkStores<FlashcardDbContext>()
+                .AddSignInManager<SignInManager<ApplicationUser>>();
+
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(Configuration["TokenKey"]));
+
+            services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters()
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = key,
+                        ValidateAudience = false,
+                        ValidateIssuer = false
+                    };
+                });
+
+            services.AddControllers(o =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                    .RequireAuthenticatedUser()
+                    .Build();
+                o.Filters.Add(new AuthorizeFilter(policy));
+            });
+
+            services.AddScoped<IJWTGenerator, JWTGenerator>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -50,6 +86,7 @@ namespace api_flashcards_dotnet
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
