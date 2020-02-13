9920a20ccf585589a83e149a58e0a379863e29a4
Dilip <dilip_agheda@yahoo.com>
2020-02-13 22:47:39 +1100

    Added a folder with a class to generate JWT tokens

---
+++ api-flashcards-dotnet/api-flashcards-dotnet/Infrastructure/IJWTGenerator.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using api_flashcards_dotnet.Models;
+
+namespace api_flashcards_dotnet.Infrastructure
+{
+    public interface IJWTGenerator
+    {
+        string CreateToken(ApplicationUser user);
+    }
+}
---
+++ api-flashcards-dotnet/api-flashcards-dotnet/Infrastructure/JWTGenerator.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using api_flashcards_dotnet.Models;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+
+namespace api_flashcards_dotnet.Infrastructure
+{
+    public class JWTGenerator : IJWTGenerator
+    {
+        private string _key;
+
+        public JWTGenerator(IConfiguration config)
+        {
+            _key = config["TokenKey"];
+        }
+        public string CreateToken(ApplicationUser user)
+        {
+            var claims = new List<Claim>() {
+                new Claim(JwtRegisteredClaimNames.Email, user.Email)
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_key));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor()
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+    }
+}
