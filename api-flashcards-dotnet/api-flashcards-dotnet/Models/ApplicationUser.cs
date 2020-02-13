edaf49f980b0ae03fe230faf0dc32845f442ac6d
Dilip <dilip_agheda@yahoo.com>
2020-02-13 22:25:07 +1100

    Added Model class for ApplicationUser for Identity

---
+++ api-flashcards-dotnet/api-flashcards-dotnet/Models/ApplicationUser.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using Microsoft.AspNetCore.Identity;
+
+namespace api_flashcards_dotnet.Models
+{
+    public class ApplicationUser: IdentityUser
+    {
+        public string DisplayName { get; set; }
+    }
+}
