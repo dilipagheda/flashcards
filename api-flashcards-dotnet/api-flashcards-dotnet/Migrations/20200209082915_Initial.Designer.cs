dfd7c9617c822b28576758df9cdb6c52fd1e2770
Dilip <dilip_agheda@yahoo.com>
2020-02-13 22:24:47 +1100

    Updated Migrations to cater for Identity

--- api-flashcards-dotnet/api-flashcards-dotnet/Migrations/20200209082915_Initial.Designer.cs
+++ api-flashcards-dotnet/api-flashcards-dotnet/Migrations/20200209082915_Initial.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using api_flashcards_dotnet.Data;
 
 namespace api_flashcards_dotnet.Migrations
 {
     [DbContext(typeof(FlashcardDbContext))]
-    partial class FlashcardDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200209082915_Initial")]
+    partial class Initial
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
