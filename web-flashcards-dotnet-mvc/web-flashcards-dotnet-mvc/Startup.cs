690588da61fd1a630ef6998a92930a3c4934fca0
Dilip <dilip_agheda@yahoo.com>
2020-02-16 09:59:45 +1100

    added cookie based authentication

--- web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/Startup.cs
+++ web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/Startup.cs
@@ -2,13 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.DependencyInjection.Extensions;
 using Microsoft.Extensions.Hosting;
 using web_flashcards_dotnet_mvc.Data;
+using web_flashcards_dotnet_mvc.Services;
+using web_flashcards_dotnet_mvc.Services.Models;
 
 namespace web_flashcards_dotnet_mvc
 {
@@ -25,7 +33,28 @@ namespace web_flashcards_dotnet_mvc
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddSingleton<IDeckData, InMemoryDeckData>();
-            services.AddControllersWithViews();
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+                    .AddCookie( options =>
+                    {
+                        options.LoginPath = "/account/login";
+                        options.LogoutPath = "/account/logout";
+                        options.AccessDeniedPath = "/account/forbidden";
+                        options.Cookie.HttpOnly = true;
+                        options.Cookie.SameSite = SameSiteMode.Strict;
+                        options.Cookie.IsEssential = true;
+                        options.Cookie.Name = CookieAuthenticationDefaults.AuthenticationScheme;
+                        //options.Cookie.Expiration = TimeSpan.FromHours(24);
+                    });
+
+            services.AddControllersWithViews(options =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                                    .RequireAuthenticatedUser()
+                                    .Build();
+                options.Filters.Add(new AuthorizeFilter(policy));
+            });
+            services.AddHttpContextAccessor();
+            services.AddScoped<FlashcardClient>( x => new FlashcardClient("https://localhost:5001", x.GetService<IHttpContextAccessor>()));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -43,9 +72,10 @@ namespace web_flashcards_dotnet_mvc
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
-
+            
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
