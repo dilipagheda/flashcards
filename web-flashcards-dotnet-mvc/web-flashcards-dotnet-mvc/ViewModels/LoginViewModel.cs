1b6f7783ea0df6d09a6401852feaf15336edf7e1
Dilip <dilip_agheda@yahoo.com>
2020-02-16 09:57:52 +1100

    added view model for login

---
+++ web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/ViewModels/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace web_flashcards_dotnet_mvc.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+    }
+}
