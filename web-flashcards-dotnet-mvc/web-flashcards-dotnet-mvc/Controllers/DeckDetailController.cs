343de7fe0140eb7928bd912246fd04752ca6ac2f
Dilip <dilip_agheda@yahoo.com>
2020-02-16 19:40:44 +1100

    DeckDetailController - got the view deck detail action method working against backend API

--- web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/Controllers/DeckDetailController.cs
+++ web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/Controllers/DeckDetailController.cs
@@ -5,6 +5,8 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using web_flashcards_dotnet_mvc.Data;
 using web_flashcards_dotnet_mvc.Models;
+using web_flashcards_dotnet_mvc.Services;
+using web_flashcards_dotnet_mvc.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
@@ -12,56 +14,73 @@ namespace web_flashcards_dotnet_mvc.Controllers
 {
     public class DeckDetailController : Controller
     {
-        private readonly IDeckData deckData;
+        private readonly FlashcardClient _flashcardClient;
 
-        public DeckDetailController(IDeckData deckData)
+        public DeckDetailController(FlashcardClient flashcardClient)
         {
-            this.deckData = deckData;
+            _flashcardClient = flashcardClient;
         }
 
-        // GET: /<controller>/
-        public ViewResult Index(int id)
+        public async Task<ViewResult> Index(int id)
         {
-            Deck deck = this.deckData.GetDecks().FirstOrDefault(deck => deck.Id == id);
-            return View(deck);
-        }
+            var deck = await _flashcardClient.GetDeckById(id);
+            var cardsByDeckId = await _flashcardClient.GetCardsByDeckId(id);
 
-        // GET
-        [HttpGet]
-        public ViewResult AddNewCard(int id)
-        {
-            Deck deck = deckData.GetDecks().FirstOrDefault(deck => deck.Id == id);
-            Card card = new Card
+            if(deck != null && cardsByDeckId != null)
             {
-                DeckId = deck.Id
-            };
-            ViewData["DeckName"] = deck.Name;
-            return View(card);
-        }
+                var deckViewModel = new DeckViewModel()
+                {
+                    Id = deck.Id,
+                    Name = deck.Name,
+                    TotalCards = deck.totalCards,
+                    Cards = cardsByDeckId.Cards
+                };
 
-        // POST
-        [HttpPost]
-        public IActionResult AddNewCard(int id,Card card)
-        {
-            Deck deck = deckData.GetDecks().FirstOrDefault(deck => deck.Id == id);
-            if (!ModelState.IsValid)
-            {
-                ViewData["DeckName"] = deck.Name;
-                return View(card);
+                return View(deckViewModel);
             }
-            card.DeckId = deck.Id;
-            deckData.AddCardToDeck(card);
-            return RedirectToAction("Index", new { id = deck.Id});
-            
+            else
+            {
+                return View("NotFound");
+            }
+
         }
 
-        // POST
-        [HttpPost]
-        public IActionResult DeleteCard(int deckId, int cardId)
-        {
-            deckData.DeleteCardFromDeck(deckId, cardId);
-            return RedirectToAction("Index", new { id = deckId });
-        }
+        //// GET
+        //[HttpGet]
+        //public ViewResult AddNewCard(int id)
+        //{
+        //    Deck deck = deckData.GetDecks().FirstOrDefault(deck => deck.Id == id);
+        //    Card card = new Card
+        //    {
+        //        DeckId = deck.Id
+        //    };
+        //    ViewData["DeckName"] = deck.Name;
+        //    return View(card);
+        //}
+
+        //// POST
+        //[HttpPost]
+        //public IActionResult AddNewCard(int id,Card card)
+        //{
+        //    Deck deck = deckData.GetDecks().FirstOrDefault(deck => deck.Id == id);
+        //    if (!ModelState.IsValid)
+        //    {
+        //        ViewData["DeckName"] = deck.Name;
+        //        return View(card);
+        //    }
+        //    card.DeckId = deck.Id;
+        //    deckData.AddCardToDeck(card);
+        //    return RedirectToAction("Index", new { id = deck.Id});
+            
+        //}
+
+        //// POST
+        //[HttpPost]
+        //public IActionResult DeleteCard(int deckId, int cardId)
+        //{
+        //    deckData.DeleteCardFromDeck(deckId, cardId);
+        //    return RedirectToAction("Index", new { id = deckId });
+        //}
 
     }
 }
