e194ffb76f2ba0a57e12053b8e92168e388358aa
Dilip <dilip_agheda@yahoo.com>
2020-02-16 19:37:57 +1100

    Removed InMemory instance

--- web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/Startup.cs
+++ web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/Startup.cs
@@ -32,7 +32,6 @@ namespace web_flashcards_dotnet_mvc
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddSingleton<IDeckData, InMemoryDeckData>();
             services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
                     .AddCookie( options =>
                     {
