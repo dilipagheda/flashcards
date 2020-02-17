63d8971cf890da40b12441e2131dfff568f89ae4
Dilip <dilip_agheda@yahoo.com>
2020-02-17 21:07:54 +1100

    Added View models for Card and Deck

---
+++ web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/ViewModels/CardViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace web_flashcards_dotnet_mvc.ViewModels
+{
+    public class CardViewModel
+    {
+        public int DeckId { get; set; }
+        public string DeckName { get; set; }
+
+        [Required]
+        [DataType(DataType.Text)]
+        [StringLength(500, MinimumLength = 5)]
+        public string QuestionText { get; set; }
+
+        [Required]
+        [StringLength(500,MinimumLength =5)]
+        [DataType(DataType.Text)]
+        public string AnswerText { get; set; }
+    }
+}
---
+++ web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/ViewModels/DeckViewModel.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using web_flashcards_dotnet_mvc.Services.Models;
+
+namespace web_flashcards_dotnet_mvc.ViewModels
+{
+    public class DeckViewModel
+    {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string Name { get; set; }
+
+        public int TotalCards { get; set; }
+
+        public List<Card> Cards { get; set; }
+    }
+
+}
