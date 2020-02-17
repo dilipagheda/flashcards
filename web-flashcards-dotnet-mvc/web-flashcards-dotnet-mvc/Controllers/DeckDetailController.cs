bf0c1a859403e0b8334679f96f36aa7345d46826
Dilip <dilip_agheda@yahoo.com>
2020-02-17 21:08:19 +1100

    Completed core functionality of DeckDetailController

--- web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/Controllers/DeckDetailController.cs
+++ web-flashcards-dotnet-mvc/web-flashcards-dotnet-mvc/Controllers/DeckDetailController.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using web_flashcards_dotnet_mvc.Data;
 using web_flashcards_dotnet_mvc.Models;
 using web_flashcards_dotnet_mvc.Services;
+using web_flashcards_dotnet_mvc.Services.Models;
 using web_flashcards_dotnet_mvc.ViewModels;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
@@ -42,45 +43,66 @@ namespace web_flashcards_dotnet_mvc.Controllers
             {
                 return View("NotFound");
             }
+        }
+
+        // GET
+        [HttpGet]
+        public async Task<ViewResult> AddNewCard(int id)
+        {
+            var deck = await _flashcardClient.GetDeckById(id);
+            var cardViewModel = new CardViewModel()
+            {
+                DeckId = deck.Id,
+                DeckName = deck.Name
+            };
+
+            return View(cardViewModel);
+        }
+
+        // POST
+        [HttpPost]
+        public async Task<IActionResult> AddNewCard(int id, CardViewModel cardViewModel)
+        {
+            var deck = await _flashcardClient.GetDeckById(id);
+            cardViewModel.DeckId = deck.Id;
+            cardViewModel.DeckName = deck.Name;
+            if (!ModelState.IsValid)
+            {
+                return View(cardViewModel);
+            }
+
+            var response = await _flashcardClient.CreateCardByDeckId(id, cardViewModel.QuestionText, cardViewModel.AnswerText);
+
+            if(!response.IsSuccess)
+            {
+                foreach(var err in response.Errors.ErrorItems.QuestionText)
+                {
+                    ModelState.AddModelError(err, err);
+                }
+                foreach (var err in response.Errors.ErrorItems.AnswerText)
+                {
+                    ModelState.AddModelError(err, err);
+                }
+                ModelState.AddModelError("errorCreation", "Problem creating card");
+                return View(cardViewModel);
+            }
+
+            return RedirectToAction("Index", new { id = deck.Id });
 
         }
 
-        //// GET
-        //[HttpGet]
-        //public ViewResult AddNewCard(int id)
-        //{
-        //    Deck deck = deckData.GetDecks().FirstOrDefault(deck => deck.Id == id);
-        //    Card card = new Card
-        //    {
-        //        DeckId = deck.Id
-        //    };
-        //    ViewData["DeckName"] = deck.Name;
-        //    return View(card);
-        //}
+        // POST
+        [HttpPost]
+        public async Task<IActionResult> DeleteCard(int deckId, int cardId)
+        {
+            var response = await _flashcardClient.DeleteCardByDeckId(deckId, cardId);
 
-        //// POST
-        //[HttpPost]
-        //public IActionResult AddNewCard(int id,Card card)
-        //{
-        //    Deck deck = deckData.GetDecks().FirstOrDefault(deck => deck.Id == id);
-        //    if (!ModelState.IsValid)
-        //    {
-        //        ViewData["DeckName"] = deck.Name;
-        //        return View(card);
-        //    }
-        //    card.DeckId = deck.Id;
-        //    deckData.AddCardToDeck(card);
-        //    return RedirectToAction("Index", new { id = deck.Id});
-            
-        //}
-
-        //// POST
-        //[HttpPost]
-        //public IActionResult DeleteCard(int deckId, int cardId)
-        //{
-        //    deckData.DeleteCardFromDeck(deckId, cardId);
-        //    return RedirectToAction("Index", new { id = deckId });
-        //}
+            if(!response.IsSuccess)
+            {
+                return NotFound();
+            }
+            return RedirectToAction("Index", new { id = deckId });
+        }
 
     }
 }
